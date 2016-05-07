a9d95d7cf89429474efb7d7671ae4216b63730e7
Martone Rosario <rosario.martone@gmail.com>
2016-05-07 17:28:45 +0100

    v 1.0.0

---
+++ WcfServiceAdvertiseMe/IServiceAdvertiseMe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using Entities;
+using System.Text;
+
+namespace WcfServiceAdvertiseMe
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServiceAdvertiseMe" in both code and config file together.
+    [ServiceContract]
+    public interface IServiceAdvertiseMe
+    {
+        [OperationContract]
+        [WebInvoke(Method ="GET", UriTemplate = "/PointsByGPSPosition/{gpsPosition}", RequestFormat =WebMessageFormat.Json,ResponseFormat =WebMessageFormat.Json,BodyStyle =WebMessageBodyStyle.Wrapped)]
+        List<Pub> GetPointsByGPSPosition(string gpsPosition);
+    }
+}
---
+++ WcfServiceAdvertiseMe/ServiceAdvertiseMe.svc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceAdvertiseMe
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServiceAdvertiseMe" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ServiceAdvertiseMe.svc or ServiceAdvertiseMe.svc.cs at the Solution Explorer and start debugging.
+    public class ServiceAdvertiseMe : IServiceAdvertiseMe
+    {
+        public List<Pub> GetPointsByGPSPosition(string gpsPosition)
+        {
+            Pub pub = new Pub();
+            List<Pub> pubs = new List<Pub>();
+            pubs.Add(pub);
+
+            return pubs;
+        }
+    }
+}
